df729ad5080eaee317bf12ff52766719f5c5fa76
nenechidiebere <nenebrown024@gmail.com>
2022-09-15 20:54:44 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints numbers between 0 to
+ * print_most_numbers - prints numbers between 0 to 9
  * (neither 2 nor 4).
  * Return: no return.
  */
@@ -16,3 +16,4 @@ void print_most_numbers(void)
 			_putchar(ch);
 	}
 	_putchar('\n');
+}
